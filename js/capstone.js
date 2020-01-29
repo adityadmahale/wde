7b9615898b7afe850728322622dcadc6870e3c32
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-01-29 15:32:25 +0530

    Add mobile menu

--- js/capstone.js
+++ js/capstone.js
@@ -16,3 +16,12 @@ function displayModal(imgElement){
 function closeModal() {
   modal.style.display = "none";
 }
+
+function myFunction() {
+  var x = document.getElementById("navlinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
